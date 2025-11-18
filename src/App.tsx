9567db0363132a44eac9821b888e7977ecf30e76
noloerino <jhshi07@gmail.com>
2025-11-17 19:55:22 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Jonathan Shi</h1>
+      website very under construction! check back later. or maybe never.
+      <h2>h2 level</h2>
+      <h3>h3 level</h3>
+      <p>sample text</p>
+    </>
+  );
+}
+
+export default App;
